3494de06d64f8452431622ff9747e84700cb9065
victorcsar <victorcesagx@gmail.com>
2024-04-03 11:35:11 -0300

    temp 1:42:13

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import AuthSocialButton from "./AuthSocialButton"
 import{ useCallback, useState } from "react";
 import {FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
+import axios from "axios";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -39,7 +40,7 @@ const AuthForm = () =>{
         setIsLoading(true);
 
         if(variant === 'REGISTER'){
-
+            axios.post('/api/register', data);
         }
         if(variant === 'LOGIN'){
 
--- app/api/register/router.ts
+++
@@ -1,3 +0,0 @@
-import bcrypt from "bcrypt"
-
-import prisma from "@/app/libs/prismadb"
