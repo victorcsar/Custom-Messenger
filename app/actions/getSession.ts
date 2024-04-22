cd0a9add80b2e50d6cbfd9b4d42bc25783d223be
victorcsar <victorcesagx@gmail.com>
2024-04-22 09:22:06 -0300

    ATT import

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/libs/Auth";
 
 export default async function getSession() {
     return await getServerSession(authOptions);
